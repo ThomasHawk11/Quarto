1deb86f92e7b143fc54a78b668b4d1f5b1a55b20
ThomasHawk11 <94489158+ThomasHawk11@users.noreply.github.com>
2022-04-07 22:51:26 +0200

    Add files via upload

---
+++ board.c
@@ -0,0 +1,367 @@
+//QUARTO : Moteur - PLANTAIS Thomas | LESIEUR Romain TD3
+
+//Directives de préprocesseur
+#include <stdio.h>
+#include <stdlib.h>
+#include "board.h"
+
+//Création des fonctions
+
+//Définition du tab de jeu
+struct board_t{
+  piece tab[DIMENSION][DIMENSION];
+  int ligne_last_piece;
+  int colonne_last_piece;
+};
+
+//Définition d'une pièce
+struct piece_t{
+  enum size taille;
+  enum shape forme;
+  enum color couleur;
+  enum top dessus;
+};
+
+//Création d'un nouveau tab
+board new_game(){
+  board b;
+  b = malloc(sizeof(struct board_t));
+  for(int i = 0 ; i < DIMENSION ; i++){
+    for(int j = 0 ; j < DIMENSION ; j++){
+      b->tab[i][j] = NULL ;
+    }
+  }
+  b->ligne_last_piece = -1;
+  b->colonne_last_piece = -2;
+  return b;
+}
+
+//Copie d'une tab
+board copy_game(board game){
+    board copy = new_game();
+    
+    copy->ligne_last_piece = game->ligne_last_piece;
+    copy->colonne_last_piece = game->colonne_last_piece;
+    
+    for(int i = 0 ; i < DIMENSION ; i++){
+        for(int j = 0 ; j < DIMENSION ; j++){
+            if(game->tab[i][j] != NULL){
+                copy->tab[i][j] = game->tab[i][j];
+            }
+        }
+    }
+    return copy;
+}
+
+//Suppression du tab
+void destroy_game(board game){
+  free(game);
+}
+
+//Renvoie "true" si la case est occupée
+bool is_occupied(board game, int line, int column){
+  if(game->tab[line][column]!=NULL){
+    return true;
+  }
+  return false;
+}
+
+//Renvoie la pièce placée sur une case du tab
+piece get_piece(board game, int line, int column){
+  if(game->tab[line][column]!=NULL || line < 0 || line > 3 || column < 0 || column > 3 ){
+    return game->tab[line][column];
+  }else{
+    return NULL;
+  }
+}
+
+//Taille de la pièce
+enum size piece_size(piece a_piece){
+  if(a_piece!=NULL){
+    return a_piece->taille;
+  }else{
+    return TALL ;
+  }
+}
+
+//Dessus de la pièce
+enum top piece_top(piece a_piece){
+  if(a_piece!=NULL){
+    return a_piece->dessus;
+  }else{
+    return HOLLOW ;
+  }
+}
+
+//Couleur de la pièce
+enum color piece_color(piece a_piece){
+  if(a_piece!=NULL){
+    return a_piece->couleur;
+  }else{
+    return RED ;
+  }
+}
+
+//Forme de la pièce
+enum shape piece_shape(piece a_piece){
+  if(a_piece!=NULL){
+    return a_piece->forme;
+  }else{
+    return SQUARE ;
+  }
+
+}
+
+/*
+bool has_winner(board game){
+  int comparaison[4]={0,0,0,0};
+   Chaque valeur du tableau représente le nombre de comparaison vraie pour chaque
+  caractéristiques. Il y aura un gagnant si l'on retrouve un 3 dans le tableau 
+  if(game->ligne_last_piece == -1){
+    return 0;
+  }else{
+    //Vérification en colonne
+    if(game->tab[0][game->colonne_last_piece]!=NULL && game->tab[1][game->colonne_last_piece]!=NULL && game->tab[2][game->colonne_last_piece]!=NULL && game->tab[3][game->colonne_last_piece]!=NULL){
+      for(int i=0 ; i < DIMENSION - 1 ; i++){
+        if(piece_size(game->tab[i][game->colonne_last_piece]) == piece_size(game->tab[i+1][game->colonne_last_piece])){
+          comparaison[0] = comparaison[0] +1;
+        }
+        else if(piece_shape(game->tab[i][game->colonne_last_piece]) == piece_shape(game->tab[i+1][game->colonne_last_piece])){
+          comparaison[1] = comparaison[1] +1;
+        }
+        else if(piece_color(game->tab[i][game->colonne_last_piece]) == piece_color(game->tab[i+1][game->colonne_last_piece])){
+          comparaison[2] = comparaison[2] +1;
+        }
+        else if(piece_top(game->tab[i][game->colonne_last_piece]) == piece_top(game->tab[i+1][game->colonne_last_piece])){
+          comparaison[3] = comparaison[3] +1;
+        }
+      }
+
+    //Vérification en ligne
+    }else if(game->tab[game->ligne_last_piece][0]!=NULL && game->tab[game->ligne_last_piece][1]!=NULL && game->tab[game->ligne_last_piece][2]!=NULL && game->tab[game->ligne_last_piece][3]!=NULL){
+      for(int i=0 ; i < DIMENSION - 1 ; i++){
+        if(piece_size(game->tab[game->ligne_last_piece][i]) == piece_size(game->tab[game->ligne_last_piece][i+1])){
+          comparaison[0] = comparaison[0] +1;
+        }
+        else if(piece_shape(game->tab[game->ligne_last_piece][i]) == piece_shape(game->tab[game->ligne_last_piece][i+1])){
+          comparaison[1] = comparaison[1] +1;
+        }
+        else if(piece_color(game->tab[game->ligne_last_piece][i]) == piece_color(game->tab[game->ligne_last_piece][i+1])){
+          comparaison[2] = comparaison[2] +1;
+        }
+        else if(piece_top(game->tab[game->ligne_last_piece][i]) == piece_top(game->tab[game->ligne_last_piece][i+1])){
+          comparaison[3] = comparaison[3] +1;
+        }
+      }
+    }
+
+    //Vérification en diagonale
+    else if(game->tab[0][0]!=NULL && game->tab[1][1]!=NULL && game->tab[2][2]!=NULL && game->tab[3][3]!=NULL ){
+      for(int i=0 ; i < DIMENSION - 1 ; i++){
+        if(piece_size(game->tab[i][i]) == piece_size(game->tab[i+1][i+1])){
+          comparaison[0] = comparaison[0] +1;
+        }
+        else if(piece_shape(game->tab[i][i]) == piece_shape(game->tab[i+1][i+1])){
+          comparaison[1] = comparaison[1] +1;
+        }
+        else if(piece_color(game->tab[i][i]) == piece_color(game->tab[i+1][i+1])){
+          comparaison[2] = comparaison[2] +1;
+        }
+        else if(piece_top(game->tab[i][i]) == piece_top(game->tab[i+1][i+1])){
+          comparaison[3] = comparaison[3] +1;
+        }
+      }
+    }
+
+    //Vérification de la deuxième diagonale
+    else if(game->tab[0][3]!=NULL && game->tab[1][2]!=NULL && game->tab[2][1]!=NULL && game->tab[3][0]!=NULL ){
+      for(int i=0 ; i < DIMENSION - 1 ; i++){
+        if(piece_size(game->tab[i][3-i]) == piece_size(game->tab[i][3-i])){
+          comparaison[0] = comparaison[0] +1;
+        }
+        else if(piece_shape(game->tab[i][3-i]) == piece_shape(game->tab[i][3-i])){
+          comparaison[1] = comparaison[1] +1;
+        }
+        else if(piece_color(game->tab[i][3-i]) == piece_color(game->tab[i][3-1])){
+          comparaison[2] = comparaison[2] +1;
+        }
+        else if(piece_top(game->tab[i][3-i]) == piece_top(game->tab[i][3-1])){
+          comparaison[3] = comparaison[3] +1;
+        }
+      }
+    }
+    for(int i=0 ; i < 4 ; i++){
+      if(comparaison[i]==3){
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+*/
+
+
+
+
+
+//Fonction qui permet savoir si il y a un gagnant selon un tab de jeu
+bool has_winner(board game){
+      int check[4]={0,0,0,0};
+    
+    //Test de la ligne
+    for(int i = 0 ; i < DIMENSION ; i++){
+        if(game->tab[game->ligne_last_piece][i] == NULL){
+            break;
+        }
+        if(game->tab[game->ligne_last_piece][i]->taille == game->tab[game->ligne_last_piece][game->colonne_last_piece]->taille){
+            check[0]++;
+        }
+        if(game->tab[game->ligne_last_piece][i]->forme == game->tab[game->ligne_last_piece][game->colonne_last_piece]->forme){
+            check[1]++;
+        }
+        if(game->tab[game->ligne_last_piece][i]->couleur == game->tab[game->ligne_last_piece][game->colonne_last_piece]->couleur){
+            check[2]++;
+        }
+        if(game->tab[game->ligne_last_piece][i]->dessus== game->tab[game->ligne_last_piece][game->colonne_last_piece]->dessus){
+            check[3]++;
+        }
+    }
+    for(int i=0 ; i < DIMENSION ; i++){
+      if(check[i]==4){
+        return 1;
+      }
+      check[i]=0;
+    }
+    
+    //Test de la colonne
+    for(int i = 0 ; i < DIMENSION ; i++){
+        if(game->tab[i][game->colonne_last_piece] == NULL){
+            break;
+        }
+        if(game->tab[i][game->colonne_last_piece]->taille == game->tab[game->ligne_last_piece][game->colonne_last_piece]->taille){
+            check[0]++;
+        }
+        if(game->tab[i][game->colonne_last_piece]->forme == game->tab[game->ligne_last_piece][game->colonne_last_piece]->forme){
+            check[1]++;
+        }
+        if(game->tab[i][game->colonne_last_piece]->couleur == game->tab[game->ligne_last_piece][game->colonne_last_piece]->couleur){
+            check[2]++;
+        }
+        if(game->tab[i][game->colonne_last_piece]->dessus== game->tab[game->ligne_last_piece][game->colonne_last_piece]->dessus){
+            check[3]++;
+        }
+    }
+    for(int i=0 ; i < DIMENSION ; i++){
+      if(check[i]==4){
+        return 1;
+      }
+      check[i]=0;
+    }
+
+    
+    //Test de la première diagonale
+    if(game->ligne_last_piece == game->colonne_last_piece){
+        
+        for(int i = 0 ; i < DIMENSION ; i++){
+            
+            if(game->tab[i][i] == NULL){
+                break;
+            }
+            if(game->tab[i][i]->taille == game->tab[game->ligne_last_piece][game->colonne_last_piece]->taille){
+                check[0]++;
+            }
+            if(game->tab[i][i]->forme == game->tab[game->ligne_last_piece][game->colonne_last_piece]->forme){
+                check[1]++;
+            }
+            if(game->tab[i][i]->couleur == game->tab[game->ligne_last_piece][game->colonne_last_piece]->couleur){
+                check[2]++;
+            }
+            if(game->tab[i][i]->dessus == game->tab[game->ligne_last_piece][game->colonne_last_piece]->dessus){
+                check[3]++;
+            }
+        }
+        
+    for(int i=0 ; i < DIMENSION ; i++){
+      if(check[i]==4){
+        return 1;
+      }
+      check[i]=0;
+    }
+  }
+    
+    //Test de la deuxième diagonale
+    if(game->ligne_last_piece == DIMENSION - game->colonne_last_piece - 1){
+       
+       for(int i = 0 ; i < DIMENSION ; i++){
+            
+            if(game->tab[DIMENSION - i - 1][i] == NULL){
+                break;
+            }
+            if(game->tab[DIMENSION - i - 1][i]->taille == game->tab[game->ligne_last_piece][game->colonne_last_piece]->taille){
+                check[0]++;
+            }
+            if(game->tab[DIMENSION - i - 1][i]->forme == game->tab[game->ligne_last_piece][game->colonne_last_piece]->forme){
+                check[1]++;
+            }
+            if(game->tab[DIMENSION - i - 1][i]->couleur == game->tab[game->ligne_last_piece][game->colonne_last_piece]->couleur){
+                check[2]++;
+            }
+            if(game->tab[DIMENSION - i - 1][i]->dessus == game->tab[game->ligne_last_piece][game->colonne_last_piece]->dessus){
+                check[3]++;
+            }
+        }
+        
+    for(int i=0 ; i < DIMENSION ; i++){
+      if(check[i]==4){
+        return 1;
+      }
+      check[i]=0;
+    }
+
+  }
+    
+    return 0;
+}
+
+
+//Renvoie "true" si la pièce est présente sur le tab
+bool is_present_on_board(board game, piece a_piece){
+  for(int i = 0 ; i < DIMENSION ; i++){
+    for(int j = 0 ; j < DIMENSION ; j++){
+      if(game->tab[i][j]!=NULL
+         && piece_size(a_piece)==piece_size(game->tab[i][j]) 
+         && piece_shape(a_piece)==piece_shape(game->tab[i][j]) 
+         && piece_color(a_piece)==piece_color(game->tab[i][j]) 
+         && piece_top(a_piece)==piece_top(game->tab[i][j])){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//Placer une pièce sur le tab
+enum return_code place_piece(board game, int line, int column, piece a_piece){
+    if(line<0 || line>=DIMENSION || column<0 || column>=DIMENSION || is_occupied(game, line, column)){
+        return POSITION;
+    }
+    else if(is_present_on_board(game, a_piece)){
+        return PIECE;
+    }
+    game->tab[line][column] = a_piece;
+    game->ligne_last_piece = line;
+    game->colonne_last_piece = column;
+    return SUCCESS;
+}
+
+
+//Création d'une pièce
+piece get_piece_from_characteristics(enum size a_size, enum shape a_shape, enum color a_color,  enum top a_top){
+  piece p;
+  p = malloc(sizeof(struct piece_t));
+  p->taille=a_size;
+  p->forme=a_shape;
+  p->couleur=a_color;
+  p->dessus=a_top;
+  return p;
+}
---
+++ main.c
@@ -0,0 +1,240 @@
+// Compilation : prompt> gcc -Wall main.c board.o -o quarto 
+// Lancement : prompt> ./quarto
+
+//QUARTO : Interface - PLANTAIS Thomas | LESIEUR Romain TD3
+
+//Directives de préprocesseur
+#include <stdio.h>
+#include <stdlib.h>
+#include "board.c"
+
+//Pré-initialisation de fonctions
+void afficher_piece(piece p);
+
+//Création des fonctions
+
+//Affichage du tableau et des pièces qu'il contient
+void affiche_grille(board game){
+  printf("    0     1     2     3\n");
+  for(int i=0 ; i < DIMENSION ; i++){
+    printf(" ╬═════╬═════╬═════╬═════╬\n");
+    printf("%d║",i);
+    for(int j=0 ; j < DIMENSION ; j++){
+      if(is_occupied(game,i,j) == 1){        //Si la case est occupée, on affiche la
+        afficher_piece(get_piece(game,i,j)); //pièce qui se trouve dessus.
+        printf("║"); 
+      }else{
+      printf("     ║");      
+      }
+    }
+    printf("\n");
+  }
+  printf(" ╬═════╬═════╬═════╬═════╬\n");
+}
+
+
+// Fonction permettant de demander à l'utilisateur de
+// choisir les caractéristiques d'une pièce.
+piece demander_piece(){
+
+  //Demande la taille de la pièce.
+  printf("\nQuelle est la taille de la pièce ?\n0 : TALL\n1 : SHORT\n");
+  int taille = 2;
+  enum size size;
+  printf("→ ");
+  while(taille !=1 && taille !=0){
+    while (scanf("%d",&taille)!= 1){
+      getchar();
+    }
+  }
+
+  if(taille == 0){
+    size = TALL;
+  }else{
+    size = SHORT;
+  }
+
+  //Demande la forme de la pièce
+  printf("\nQuelle est la forme de la pièce ?\n0 : SQUARE\n1 : CIRCULAR\n");
+  int forme = 2;
+  enum shape shape;
+  printf("→ ");
+  while(forme != 1 && forme != 0){
+    while (scanf("%d",&forme)!= 1){
+      getchar();
+    }
+  }
+
+  if(forme == 0){
+    shape = SQUARE;
+  }else{
+    shape = CIRCULAR;
+  }
+
+  //Demande la couleur de la pièce.
+  printf("\nQuelle est la couleur de la pièce ?\n0 : RED\n1 : BLUE\n");
+  int couleur = 2;
+  enum color color;
+  printf("→ ");
+  while(couleur != 1 && couleur != 0){
+    while (scanf("%d",&couleur)!= 1){
+      getchar();
+    }
+  }
+
+  if(couleur == 0){
+    color = RED;
+  }else{
+    color = BLUE;
+  }
+
+  //Demande si la pièce est pleine ou creuse.
+  printf("\nLa pièce est-elle creuse ou pleine ?\n0 : HOLLOW\n1 : SOLID\n");
+  int dessus = 2;
+  enum top top;
+  printf("→ ");
+  while(dessus != 1 && dessus != 0){
+    while (scanf("%d",&dessus)!= 1){
+      getchar();
+    }
+  }
+
+  if(dessus == 0){
+    top = HOLLOW;
+  }else{
+    top = SOLID;
+  }
+
+  //Retourne la picèe en fonction des caractéristiques choisis.
+  return get_piece_from_characteristics(size, shape, color, top);
+
+}
+
+
+// Fonction permettant de demander au joueur la position
+// à laquelle il veut placer sa pièce.
+void demander_position(board game,piece p){
+  int x;
+  int y;
+  printf("A quelle position voulez-vous mettre votre pièce ?\n");
+  printf("LIGNE : ");
+  scanf("%d",&x);
+  printf("COLONNE : ");
+  scanf("%d",&y);
+  //Vérifie si la pièce peut être placée.
+  enum return_code res = place_piece(game, x, y, p);
+  if(res == SUCCESS){
+  	printf("\nLa piece a été placée avec succès.\n\n");
+	}
+	else {
+	  printf("\nUn problème a eu lieu lors du placement\n\n");
+	}
+}
+
+// Fonction permettant d'afficher la pièce en fonction
+// des paramètres choisis.
+void afficher_piece(piece p){
+  
+  //Possibilités de caractéristiques des pièces ROUGES.
+  if(piece_color(p)==RED){
+    if(piece_size(p)==TALL){
+      printf("\033[31m █\033[m");
+    }else{
+      printf("\033[31m ▂\033[m");
+    }
+  
+    if(piece_shape(p)==SQUARE && piece_top(p)==HOLLOW){
+      printf("\033[31m ◻ \033[m");
+    }
+    else if(piece_shape(p)==SQUARE && piece_top(p)!=HOLLOW){
+      printf("\033[31m ■ \033[m");
+    }
+    else if(piece_shape(p)!=SQUARE && piece_top(p)==HOLLOW){
+      printf("\033[31m ◯ \033[m");
+    }
+    else if(piece_shape(p)!=SQUARE && piece_top(p)!=HOLLOW){
+      printf("\033[31m ⬤ \033[m");
+    }
+  }
+
+  //Possibilités de caractéristiques des pièces BLEUES.
+  if(piece_color(p)!=RED){
+    if(piece_size(p)==TALL){
+      printf("\033[34m █\033[m");
+    }else{
+      printf("\033[34m ▂\033[m");
+    }
+  
+    if(piece_shape(p)==SQUARE && piece_top(p)==HOLLOW){
+      printf("\033[34m ◻ \033[m");
+    }
+    else if(piece_shape(p)==SQUARE && piece_top(p)!=HOLLOW){
+      printf("\033[34m ■ \033[m");
+    }
+    else if(piece_shape(p)!=SQUARE && piece_top(p)==HOLLOW){
+      printf("\033[34m ◯ \033[m");
+    }
+    else if(piece_shape(p)!=SQUARE && piece_top(p)!=HOLLOW){
+      printf("\033[34m ⬤ \033[m");
+    }
+  }
+
+}
+
+// Corps principal du jeu.
+int main(int args, char **argv){
+  int continuer = 1; //Variable utilisée pour recommencer une partie.
+  while(continuer==1){
+    printf("\033[32m▀▄▀▄▀▄▀▄▀▄▀▄▀▄ QUARTO ▀▄▀▄▀▄▀▄▀▄▀▄▀▄\n\n\033[m");
+	  board game = new_game();
+	  printf("Un plateau est créé.\n\n");
+    affiche_grille(game);
+  
+    int joueur = 1;
+
+    while(has_winner(game)==0){
+      printf("\nAu tour du joueur %d de jouer.\n",joueur);
+      piece p = get_piece_from_characteristics( TALL, SQUARE, RED, HOLLOW); // Initialisation d'une pièce quelconque.
+      p = demander_piece();
+      
+      printf("\nPièce choisie : ");
+      afficher_piece(p);
+      printf("\n\n");
+      
+      demander_position(game,p);
+      
+    
+      affiche_grille(game);
+
+      // Changement de joueur
+      if(joueur==1){
+        joueur = 2;
+      }else{
+        joueur = 1;
+      }
+    
+    }
+
+    //Détermination du gagnant.
+    if(joueur==1){
+      printf("\nLe joueur 2 a gagné !\n");
+    }else{
+      printf("\nLe joueur 1 a gagné !\n");
+    }
+
+    //Possibilité de rejouer.
+    printf("\nVoulez-vous rejouer ?\n0 : NON\n1 : OUI\n");
+    printf("→ ");
+    scanf("%d",&continuer);
+    if(continuer==0){
+      printf("\nSuppression du plateau...\n");
+      destroy_game(game);
+    }
+
+  }
+  //Fin de partie.
+  printf("\033[31m\n▀▄▀▄▀▄▀▄▀ PARTIE TERMINEE ▄▀▄▀▄▀▄▀▄\n\033[m");
+	return 0;
+}
+
+
